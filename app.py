0cffcd2ca9f94e5b42bc4738d80bf586228a6240
SOEP <139007040+poepoepipi@users.noreply.github.com>
2026-06-29 20:19:43 +0200

    modified:   app.py 	renamed:    static/styles.css -> static/css/styles.css 	new file:   static/js/boot-animation.js 	new file:   templates/boot.html 	modified:   templates/dashboard.html 	modified:   templates/login.html

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def boot():
+    return render_template("boot.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
 def home():
-    return render_template("index.html")
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
