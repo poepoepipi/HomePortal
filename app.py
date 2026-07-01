c7d71b4c58152ce3803d6da41e4f79671e87dfd2
poepoepipi <139007040+poepoepipi@users.noreply.github.com>
2026-07-01 14:47:48 +0200

    modified:   ToDo.txt 	modified:   app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def home():
     return render_template("dashboard.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=9384, debug=True)
