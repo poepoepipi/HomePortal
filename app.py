77aaf22bbf54b6bcf942dadc5d63c05df3af2105
poepoepipi <139007040+poepoepipi@users.noreply.github.com>
2026-07-01 15:18:13 +0200

    modified:   app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, request, session, jsonify, redirect, render_template
 
 app = Flask(__name__)
+app.secret_key = "849242943849248hfjzhfkz39348hiezlu4294892"
+PASSWORD = "temporary"
 
 @app.route("/")
 def boot():
@@ -13,6 +15,28 @@ def login():
 @app.route("/dashboard")
 def home():
     return render_template("dashboard.html")
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    password = data.get("password")
+
+    if password == PASSWORD:
+        session["logged_in"] = True
+        return jsonify({"success": True})
+
+    return jsonify({"success": False}), 401
+
+@app.route("/api/session")
+def check_session():
+    return jsonify({
+        "logged_in": session.get("logged_in", False)
+    })
+
+@app.route("/api/logout", methods=["POST"])
+def logout():
+    session.clear()
+    return jsonify({"success": True})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9784, debug=True)
